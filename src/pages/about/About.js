9a9c6e3eb8e125a06cf065016657e7e2b95cf67a
ksanjeeb <code.sanjeeb@gmail.com>
2023-07-05 01:46:30 +0530

    #

---
+++ src/pages/about/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <h1 className="about-title">About Us</h1>
+      <p className="about-description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed lectus vel mauris fermentum faucibus eget
+        et leo. In vitae tellus quis felis interdum vehicula. Mauris laoreet elementum mauris, ut finibus tortor
+        facilisis non. Suspendisse potenti. Proin eleifend eros sed lacus tempus, eget finibus nunc tempor. Nulla
+        facilisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer
+        condimentum lacus id fringilla ullamcorper.
+      </p>
+      <p className="about-description">
+        Aenean volutpat purus ac quam vulputate malesuada. Sed ultricies quam id metus rhoncus, eget suscipit magna
+        tristique. Mauris vulputate congue mi, non interdum tortor vestibulum ac. Vivamus et quam mauris. Integer
+        lobortis neque non dolor lobortis, at congue est auctor. Phasellus in est velit. Morbi eget fermentum velit.
+        Nulla facilisi. Integer at enim eget urna vehicula faucibus. Ut pellentesque feugiat lorem at rhoncus. Sed
+        tempus sagittis velit a laoreet.
+      </p>
+    </div>
+  );
+};
+
+export default About;
